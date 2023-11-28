729778c56e6ddcedde3c839ee1fb841dbe0e0d26
kia2905 <chiaradanna29@gmail.com>
2023-11-28 08:49:48 +0100

    Add input components

--- app/(profile)/private-profile/page.js
+++ app/(profile)/private-profile/page.js
@@ -1,25 +1,28 @@
-import Header from "../../components/header.js";
+import { Input } from "@/components/ui/input"
 
 export default function ProfilePage() {
   return (
     <>
     <div className="flex flex-col items-center h-auto">
-      <div className="text-3xl flex justify-center">
+      <div className="text-3xl">
         Profilo
       </div>
-
-      <div className="text-xxs">
-          Name*
-      </div>
-        <input className="ml-6 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></input>
-        <input className="ml-6 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></input>
-        <input className="ml-6 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></input>
-        <input className="ml-6 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></input>
-        <input className="ml-6 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></input>
-        <input className="ml-6 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></input>
-        <input className="ml-6 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></input>
-        <input className="ml-6 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></input>
-        <input className="ml-6 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></input>
+      
+        <Input className="w-auto h-8 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></Input>
+        <Input className="w-auto h-8 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></Input>
+        <Input className="w-auto h-8 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></Input>
+        <Input className="w-auto h-8 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></Input>
+        <Input className="w-auto h-8 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></Input>
+        <Input className="w-auto h-8 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></Input>
+        <Input className="w-auto h-8 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></Input>
+        <Input className="w-auto h-8 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></Input>
+        <Input className="w-auto h-8 mb-6 mt-3 flex justify-center w64 max-w-min border-2 border-black"></Input>
+  
+    {/* <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="email">Email</Label>
+      <Input type="email" id="email" placeholder="Email" />
+    </div> */}
+        
 
       <button className="border-2 border-black">Cancel</button>
       <button className="border-2 border-black">Save</button>
