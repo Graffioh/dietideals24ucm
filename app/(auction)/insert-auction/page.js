91572bcc8ab8a65297c14a4113f257d005077869
Graffioh <bregliascuola2002@gmail.com>
2023-11-30 10:14:06 +0100

    Rearrange size and layout

--- app/(auction)/insert-auction/page.js
+++ app/(auction)/insert-auction/page.js
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea"
 
 import ComboboxCategories from "../../components/comboboxCategories"
 import ComboboxAuctions from "../../components/comboboxAuctions.js"
@@ -11,8 +12,8 @@ import ComboboxQuality from "../../components/comboboxQualities.js"
 export default function InsertAuctionPage() {
   return (
     <>
-      <div className="flex justify-center mt-12">
-        <div className="m-6 grid grid-cols-2 gap-2 w-96 max-w-screen h-96 max-h-screen">
+      <div className="flex mt-12">
+        <div className="m-6 mr-20 ml-48 grid grid-cols-2 gap-2 w-96 max-w-screen h-96 max-h-screen">
           <button className="mt-2 bg-indigo-950 text-white rounded p-3">
             +
           </button>
@@ -24,7 +25,8 @@ export default function InsertAuctionPage() {
           </button>
         </div>
 
-        <div className="mt-8 flex flex-col space-y-6">
+      <div className="flex flex-col">
+        <div className="w-72 min-w-screen mt-8 flex flex-col space-y-6">
           <div>
             <Label>Title*</Label>
             <Input type="text" placeholder="Title" className=""></Input>
@@ -32,15 +34,18 @@ export default function InsertAuctionPage() {
 
           <div>
             <Label>Description*</Label>
-            <Input type="text" placeholder="Description" className=""></Input>
+            <Textarea placeholder="Type your description here." />
           </div>
+        </div>
 
+        <div className="mt-8 flex flex-col space-y-6">
           <ComboboxCategories></ComboboxCategories>
           <ComboboxAuctions></ComboboxAuctions>
           <ComboboxQuality></ComboboxQuality>
 
-          <Button className="">Create Auction</Button>
+          <Button className="p-6 text-lg">Create Auction</Button>
         </div>
+      </div>
       </div>
     </>
   );
