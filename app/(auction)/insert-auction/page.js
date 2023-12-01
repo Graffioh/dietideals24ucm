35baee9a43883a298f70675a97c0ba874bfd3e27
Graffioh <bregliascuola2002@gmail.com>
2023-12-01 14:35:08 +0100

    Improve insert-auction UI

--- app/(auction)/insert-auction/page.js
+++ app/(auction)/insert-auction/page.js
@@ -1,51 +1,59 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea"
+import { Textarea } from "@/components/ui/textarea";
 
-import ComboboxCategories from "../../components/comboboxCategories"
-import ComboboxAuctions from "../../components/comboboxAuctions.js"
-import ComboboxQuality from "../../components/comboboxQualities.js"
+import ComboboxCategories from "../../components/comboboxCategories";
+import ComboboxAuctions from "../../components/comboboxAuctions.js";
+import ComboboxQuality from "../../components/comboboxQualities.js";
 
 export default function InsertAuctionPage() {
   return (
     <>
-      <div className="flex mt-12">
-        <div className="m-6 mr-20 ml-48 grid grid-cols-2 gap-2 w-96 max-w-screen h-96 max-h-screen">
-          <button className="mt-2 bg-indigo-950 text-white rounded p-3">
+      <div className="flex flex-col md:flex-row items-center mt-12">
+        <div className="mx-3 mb-6 md:m-6 md:mr-20 md:ml-48 grid md:grid-cols-2 gap-2">
+          <button className="w-52 h-52 mt-2 text-2xl bg-indigo-950 text-white rounded p-3">
             +
           </button>
-          <button className="mt-2 bg-indigo-950 text-white rounded p-3">
+          <button className="w-52 h-52 mt-2 text-2xl bg-indigo-950 text-white rounded p-3">
             +
           </button>
-          <button className="mt-2 bg-indigo-950 text-white rounded p-3">
-            + 
+          <button className="w-52 h-52 mt-2 text-2xl bg-indigo-950 text-white rounded p-3">
+            +
           </button>
         </div>
 
-      <div className="flex flex-col">
-        <div className="w-72 min-w-screen mt-8 flex flex-col space-y-6">
-          <div>
-            <Label>Title*</Label>
-            <Input type="text" placeholder="Title" className=""></Input>
+        <div className="flex flex-col mx-2">
+          <div className="w-72 min-w-screen md:mt-8 flex flex-col space-y-6">
+            <div>
+              <div className="flex mb-2">
+                <Label>
+                  Title<span className="text-red-500">*</span>
+                </Label>
+              </div>
+              <Input type="text" placeholder="Title" className=""></Input>
+            </div>
+
+            <div>
+              <div className="mb-2">
+                <Label>
+                  Description<span className="text-red-500">*</span>
+                </Label>
+              </div>
+              <Textarea placeholder="Type your description here." />
+            </div>
           </div>
 
-          <div>
-            <Label>Description*</Label>
-            <Textarea placeholder="Type your description here." />
+          <div className="mt-8 flex flex-col space-y-6">
+            <ComboboxCategories></ComboboxCategories>
+            <ComboboxAuctions></ComboboxAuctions>
+            <ComboboxQuality></ComboboxQuality>
+
+            <Button className="p-7 text-lg">Create Auction</Button>
           </div>
         </div>
-
-        <div className="mt-8 flex flex-col space-y-6">
-          <ComboboxCategories></ComboboxCategories>
-          <ComboboxAuctions></ComboboxAuctions>
-          <ComboboxQuality></ComboboxQuality>
-
-          <Button className="p-6 text-lg">Create Auction</Button>
-        </div>
-      </div>
       </div>
     </>
   );
