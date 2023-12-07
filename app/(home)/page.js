ca3dbe00762ffadd86c5cd1f72066ffb7f238ddf
kia2905 <chiaradanna29@gmail.com>
2023-12-07 09:29:01 +0100

    create auction card

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import CardAuction from "../components/cardAuction";
 
 export default function Home() {
   return (
@@ -29,7 +30,10 @@ export default function Home() {
 
 
       <div className="flex justify-center font-extrabold text-2xl">
-        HOME content
+        
+        <CardAuction>
+          
+        </CardAuction>
       </div>
     </>
   );
