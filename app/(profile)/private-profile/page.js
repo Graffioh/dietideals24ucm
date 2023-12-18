ecfcb04dab5773ea3b0e7af05cddb4b44b03119f
Graffioh <bregliascuola2002@gmail.com>
2023-12-18 21:20:46 +0100

    Add working POST to DB when creating an account

--- app/(profile)/private-profile/page.js
+++ app/(profile)/private-profile/page.js
@@ -4,10 +4,66 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { hash } from "bcryptjs";
 
 export default function ProfilePage({ searchParams }) {
+  async function createUserAccount(user) {
+    console.log("og user: ", user);
+
+    try {
+      const hashedPassword = await hash(user.password, 10);
+
+      const userWithHashedPassword = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.username,
+        password: hashedPassword,
+        birthDate: user.birthDate,
+        mail: user.mail,
+      }
+        
+      const response = await fetch("http://localhost:8080/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userWithHashedPassword),
+      });
+    } catch (e) {
+      console.log({ e });
+    }
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    const inputs = event.currentTarget;
+    const userInfoFromInputs = {
+      id: 999,
+      firstName: inputs.firstName.value,
+      lastName: inputs.lastName.value,
+      username: inputs.username.value,
+      password: inputs.password.value,
+      birthDate: inputs.birthDate.value,
+      mail: inputs.mail.value,
+    };
+
+    // distinguish between POST and UPDATE
+    // UPDATE = 
+    //  1) fetch all users and check if the username in the input field is already in the database
+    //  1.1) check if username in input field == username of the current logged account
+    // else POST
+
+    // UPDATE
+
+    // POST
+    await createUserAccount(userInfoFromInputs);
+
+    console.log("form submitted");
+  }
+
   return (
     <>
       <div className="flex flex justify-center mb-10 mt-10">
@@ -17,120 +73,123 @@ export default function ProfilePage({ searchParams }) {
         </Avatar>
       </div>
 
-      <div className="flex flex-col items-center">
-        <div className="w-64 items-center grid gap-6">
-          <div>
-            <Label className="flex mb-2">
-              Name<div className="text-red-500">*</div>
-            </Label>
-            <Input
-              className="h-9 bg-white"
-              type="text"
-              id="name"
-              placeholder="Name"
-            />
+      <form onSubmit={onSubmit}>
+        <div className="flex flex-col items-center">
+          <div className="w-64 items-center grid gap-6">
+            <div>
+              <Label className="flex mb-2">
+                Name<div className="text-red-500">*</div>
+              </Label>
+              <Input
+                className="h-9 bg-white"
+                type="text"
+                id="firstName"
+                placeholder="Name"
+              />
+            </div>
+
+            <div>
+              <Label className="flex mb-2">
+                Surname<div className="text-red-500">*</div>
+              </Label>
+              <Input
+                className="h-9 bg-white"
+                type="text"
+                id="lastName"
+                placeholder="Surname"
+              />
+            </div>
+
+            <div>
+              <Label className="flex mb-2">
+                Username<div className="text-red-500">*</div>
+              </Label>
+              <Input
+                className="h-9 bg-white"
+                type="text"
+                id="username"
+                placeholder="Username"
+              />
+            </div>
+
+            <div>
+              <Label className="flex mb-2">
+                Mail<div className="text-red-500">*</div>
+              </Label>
+              <Input
+                className="h-9 bg-white"
+                type="email"
+                id="mail"
+                placeholder="Mail"
+                defaultValue={searchParams.email}
+              />
+            </div>
+
+            <div>
+              <Label className="flex mb-2">
+                Password<div className="text-red-500">*</div>
+              </Label>
+              <Input
+                className="h-9 bg-white"
+                type="password"
+                id="password"
+                placeholder="Password"
+                value={searchParams.password}
+              />
+            </div>
+
+            <div>
+              <Label className="flex mb-2">
+                Date of birth<div className="text-red-500">*</div>
+              </Label>
+              <Input
+                className="h-9 bg-white"
+                type="text"
+                id="birthDate"
+                placeholder="Date"
+              />
+            </div>
+
+            <div>
+              <Label className="flex mb-2">Phone Number</Label>
+              <Input
+                className="h-9 bg-white"
+                type="tel"
+                id="phoneNumber"
+                placeholder="Phone Number"
+              />
+            </div>
+
+            <div>
+              <Label className="flex mb-2">Bio</Label>
+              <Textarea
+                className="bg-white"
+                placeholder="Type your description here."
+                id="biography"
+              />
+            </div>
+
+            <div>
+              <Label className="flex mb-2">Website</Label>
+              <Input
+                className="h-9 bg-white"
+                type="url"
+                id="website"
+                placeholder="Website"
+              />
+            </div>
           </div>
 
-          <div>
-            <Label className="flex mb-2">
-              Surname<div className="text-red-500">*</div>
-            </Label>
-            <Input
-              className="h-9 bg-white"
-              type="text"
-              id="surname"
-              placeholder="Surname"
-            />
-          </div>
-
-          <div>
-            <Label className="flex mb-2">
-              Username<div className="text-red-500">*</div>
-            </Label>
-            <Input
-              className="h-9 bg-white"
-              type="text"
-              id="username"
-              placeholder="Username"
-            />
-          </div>
-
-          <div>
-            <Label className="flex mb-2">
-              Mail<div className="text-red-500">*</div>
-            </Label>
-            <Input
-              className="h-9 bg-white"
-              type="email"
-              id="mail"
-              placeholder="Mail"
-              defaultValue={searchParams.email}
-            />
-          </div>
-
-          <div>
-            <Label className="flex mb-2">
-              Password<div className="text-red-500">*</div>
-            </Label>
-            <Input
-              className="h-9 bg-white"
-              type="password"
-              id="password"
-              placeholder="Password"
-              value={searchParams.password}
-            />
-          </div>
-
-          <div>
-            <Label className="flex mb-2">
-              Date of birth<div className="text-red-500">*</div>
-            </Label>
-            <Input
-              className="h-9 bg-white"
-              type="text"
-              id="dateofbirth"
-              placeholder="Date"
-            />
-          </div>
-
-          <div>
-            <Label className="flex mb-2">Phone Number</Label>
-            <Input
-              className="h-9 bg-white"
-              type="tel"
-              id="phoneNumber"
-              placeholder="Phone Number"
-            />
-          </div>
-
-          <div>
-            <Label className="flex mb-2">Bio</Label>
-            <Textarea
-              className="bg-white"
-              placeholder="Type your description here."
-            />
-          </div>
-
-          <div>
-            <Label className="flex mb-2">Website</Label>
-            <Input
-              className="h-9 bg-white"
-              type="url"
-              id="website"
-              placeholder="Website"
-            />
+          <div className="flex">
+            <div className="mx-5 mb-10">
+              <Button className="mr-6 mt-6">Cancel</Button>
+            </div>
+            <div className="mx-2">
+              <Button className="mt-6">Save</Button>
+            </div>
           </div>
         </div>
-
-        <div className="flex">
-          <div className="mx-5 mb-10">
-            <Button className="mr-6 mt-6">Cancel</Button>
-          </div>
-          <div className="mx-2">
-            <Button className="mt-6">Save</Button>
-          </div>
-        </div>
-      </div>
+      </form>
     </>
   );
 }
--- server-dietideals24/src/main/java/com/ucm/serverdietideals24/APIController.java
+++ server-dietideals24/src/main/java/com/ucm/serverdietideals24/APIController.java
@@ -4,9 +4,14 @@ import com.ucm.serverdietideals24.Models.*;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,27 +19,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-
-
-
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class APIController {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    @PostMapping("/register")
-    public UserAccount createUserAccount(@RequestBody UserAccount entity) {
-        jdbcTemplate.execute("INSERT INTO useraccount ");
-        
-        return entity;
+    @GetMapping("/users")
+    public List<UserAccount> fetchAllUsers() {
+        return jdbcTemplate.query("SELECT * FROM useraccount",
+                new BeanPropertyRowMapper<UserAccount>(UserAccount.class));
     }
-    
+
+    @PostMapping("/register")
+    public ResponseEntity<UserAccount> createUserAccount(@RequestBody UserAccount entity) {
+        jdbcTemplate.execute("INSERT INTO useraccount VALUES('" + entity.getId() + "', '" + entity.getFirstName()
+                + "', '" + entity.getLastName() + "', '" + entity.getUsername() + "', '" + entity.getPassword() + "', '"
+                + entity.getBirthDate() + "', '" + entity.getMail() + "')");
+
+        return new ResponseEntity<UserAccount>(entity, HttpStatus.CREATED);
+    }
+
     @PutMapping("/update-profile/{id}")
     public UserAccount updateUserAccount(@PathVariable String id, @RequestBody UserAccount entity) {
-        
 
         return entity;
     }
-    
+
 }
