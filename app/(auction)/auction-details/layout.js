ba86fc70177f4435d5660553255fbbf5fe4bfdec
Graffioh <bregliascuola2002@gmail.com>
2024-01-12 13:38:31 +0100

    Auction details header empty when not logged

--- app/(auction)/auction-details/layout.js
+++ app/(auction)/auction-details/layout.js
@@ -3,6 +3,8 @@ import "../../globals.css";
 import Header from "../../components/header.js";
 import Footer from "../../components/footer.js";
 
+import { cookies } from "next/headers";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,12 +12,25 @@ export const metadata = {
   description: "Auction details page for DietiDeals24UCM",
 };
 
-export default function AuctionDetailsLayout({ children}) {
+export default async function AuctionDetailsLayout({ children }) {
+  function getTokenFromCookie() {
+    const nextCookies = cookies();
+
+    const tokenCookieStr = nextCookies.has("token")
+      ? nextCookies.get("token").value
+      : '"no-token"';
+
+    // return token without "..."
+    return tokenCookieStr.replaceAll('"', "");
+  }
+
+  const token = getTokenFromCookie();
+
   return (
     <div className="relative flex min-h-screen flex-col">
-      <Header headerType={"headerLoggedPartial"} />
+      <Header headerType={token === "no-token" ? "headerEmpty" : "headerLoggedPartial"} />
       <main className="flex-1">{children}</main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
