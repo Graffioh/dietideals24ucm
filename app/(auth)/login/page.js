ce2041c43e648fe96b8125f5fb20e91ab884f503
Graffioh <bregliascuola2002@gmail.com>
2024-02-06 13:42:43 +0100

    Escape ' char in login page

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -12,7 +12,7 @@ export default function LoginPage() {
         <UserAuthForm className="w-80 min-w-screen" createOrLogin={"Log In"} />
         <div className="flex">
           <p className="text-xs text-slate-500 mt-2">
-            If you don't have an account yet,
+            If you don&apos;t have an account yet,
           </p>
           <Link href="/create-account" className="font-bold text-xs mt-2 ml-1">
             create a new one here.
