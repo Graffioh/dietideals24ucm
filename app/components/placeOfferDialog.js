ae9cf9f823af6a4766b4b094494945e7a8b79062
aruta <matteoaruta6@gmail.com>
2024-02-24 11:22:01 +0100

    add constraint max current offer

--- app/components/placeOfferDialog.js
+++ app/components/placeOfferDialog.js
@@ -1,5 +1,5 @@
-'use client'
-import { useRef } from 'react';
+"use client";
+import { useState, useRef } from "react";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -15,60 +15,89 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export default function PlaceOfferDialog({ auction }) {
-  // Create a reference to the offerAmount input
+  const [isDialogOpen, setDialogOpen] = useState(false);
+
   const offerAmountRef = useRef(null);
 
   async function onSubmit(event) {
     event.preventDefault();
 
-    // Get the value from the offerAmount input using the reference
     const offerAmount = offerAmountRef.current.value;
-
     const offerFromInputs = {
       id: Date.now(),
       offerAmount: offerAmount,
       idUserAccount: auction.idUserAccount,
-      idAuction: auction.id
+      idAuction: auction.id,
     };
 
     console.log(offerFromInputs);
 
-    const insertOfferResponse = await fetch(
-      "http://localhost:8080/insert-offer",
-      {
+    if (auction.currentOffer < offerAmount) {
+      alert("La tua offerta é stata piazzata correttamente");
+      await fetch("http://localhost:8080/insert-offer", {
         method: "POST",
         body: JSON.stringify(offerFromInputs),
         headers: { "Content-Type": "application/json" },
-      }
-    );
+      });
 
-    
-    // window.location.href = "/home";
+      await fetch(
+        "http://localhost:8080/set-auction-currentoffer?id=" +
+          auction.id +
+          "&newCurrentOffer=" +
+          offerAmount,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      console.log(auction.currentOffer);
+
+      setDialogOpen(false);
+    } else {
+      alert("Fai un offerta maggiore dell'offerta corrente");
+    }
   }
 
+  const openDialog = () => {
+    setDialogOpen(true);
+  };
+
+  const closeDialog = () => {
+    setDialogOpen(false);
+  };
+
   return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button variant="default" className="p-7 text-lg">
-          Place Offer
-        </Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent className="w-64 flex flex-col items-center">
-        <AlertDialogHeader>
-          <AlertDialogTitle>Place an offer</AlertDialogTitle>
-          <AlertDialogDescription>
-            Current max offer: ...
-          </AlertDialogDescription>
-          <div className="flex justify-center">
-            {/* Attach the reference to the input */}
-            <Input ref={offerAmountRef} type="number" className="w-48" />
-          </div>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction onClick={onSubmit}>Place</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+    <>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button
+            variant="default"
+            className="p-7 text-lg"
+            onClick={openDialog}
+          >
+            Place Offer
+          </Button>
+        </AlertDialogTrigger>
+        {isDialogOpen && (
+          <AlertDialogContent className="w-64 flex flex-col items-center">
+            <AlertDialogHeader>
+              <AlertDialogTitle>Place an offer</AlertDialogTitle>
+              <AlertDialogDescription>
+                Current max offer: {auction.currentOffer} €
+              </AlertDialogDescription>
+              <div className="flex justify-center">
+                <Input ref={offerAmountRef} type="number" className="w-48" />
+              </div>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel onClick={closeDialog}>
+                Cancel
+              </AlertDialogCancel>
+              <AlertDialogAction onClick={onSubmit}>Place</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        )}
+      </AlertDialog>
+    </>
   );
 }
