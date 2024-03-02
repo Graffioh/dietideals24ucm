e7ef3d3517b6c1adeab463d695966788c2e206b2
aruta <matteoaruta6@gmail.com>
2024-03-02 10:01:53 +0100

    add scrollable functionality in CommandGroup component for category filter

--- app/components/comboboxCategories.js
+++ app/components/comboboxCategories.js
@@ -20,6 +20,10 @@ import {
 
 const categories = [
   {
+    value: "",
+    label: "All categories",
+  },
+  {
     value: "electronics",
     label: "Electronics",
   },
@@ -34,6 +38,90 @@ const categories = [
   {
     value: "stamps",
     label: "Stamps",
+  },
+  {
+    value: "beautyAndHealth",
+    label: "Beauty and Health",
+  },
+  {
+    value: "booksAndMagazines",
+    label: "Books and Magazines",
+  },
+  {
+    value: "boatingAndBoats",
+    label: "Boating and Boats",
+  },
+  {
+    value: "carsMotorcyclesAndOtherVehicles",
+    label: "Cars, Motorcycles, and Other Vehicles",
+  },
+  {
+    value: "clothingAndAccessories",
+    label: "Clothing and Accessories",
+  },
+  {
+    value: "collectibles",
+    label: "Collectibles",
+  },
+  {
+    value: "coinsAndBanknotes",
+    label: "Coins and Banknotes",
+  },
+  {
+    value: "comics",
+    label: "Comics",
+  },
+  {
+    value: "computers",
+    label: "Computers",
+  },
+  {
+    value: "homeFurnitureAndDIY",
+    label: "Home Furniture and DIY",
+  },
+  {
+    value: "infancy",
+    label: "Infancy",
+  },
+  {
+    value: "lighting",
+    label: "Lighting",
+  },
+  {
+    value: "moviesAndDVDs",
+    label: "Movies and DVDs",
+  },
+  {
+    value: "musicCDsAndVinyl",
+    label: "Music CDs and Vinyl",
+  },
+  {
+    value: "photographyAndVideo",
+    label: "Photography and Video",
+  },
+  {
+    value: "sportsAndLeisure",
+    label: "Sports and Leisure",
+  },
+  {
+    value: "ticketsAndEvents",
+    label: "Tickets and Events",
+  },
+  {
+    value: "toys",
+    label: "Toys",
+  },
+  {
+    value: "travelAccessories",
+    label: "Travel Accessories",
+  },
+  {
+    value: "videoGamesAndConsoles",
+    label: "Video Games and Consoles",
+  },
+  {
+    value: "watchesAndJewelry",
+    label: "Watches and Jewelry",
   },
 ]
 
@@ -50,6 +138,7 @@ export default function ComboboxCategories({ onCategoryChange }) {
 
       const auctionsData = await auctionsResponse.json();
       setFilteredAuctions(auctionsData);
+      onCategoryChange("All categories");
     } else {
       const filteredAuctionsResponse = await fetch(
         process.env.NEXT_PUBLIC_BASEURL + "/auctions/category",
