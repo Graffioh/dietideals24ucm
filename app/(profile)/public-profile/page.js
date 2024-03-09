e8b992aaf396e858b2dd563dcd8d9b52db290069
Graffioh <bregliascuola2002@gmail.com>
2024-03-09 13:07:54 +0100

    Add dots to show more details in public profile

--- app/(profile)/public-profile/page.js
+++ app/(profile)/public-profile/page.js
@@ -1,15 +1,12 @@
 import { Textarea } from "@/components/ui/textarea";
-import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Pencil1Icon } from "@radix-ui/react-icons";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-import AuctionPagination from "../../components/auctionPagination";
 import getCurrentUserServer from "@/app/(auth)/getCurrentUserServer";
-import CardAuction from "@/app/components/cardAuction";
 import AuctionsContainerPublicProfile from "@/app/components/auctionsContainerPublicProfile";
+import ShowMoreDetailsPublicProfile from "@/app/components/showMoreDetailsPublicProfile";
 
 export default async function ProfilePage({ searchParams }) {
   const currentUser = await getCurrentUserServer();
@@ -42,42 +39,46 @@ export default async function ProfilePage({ searchParams }) {
   return (
     <>
       <div className="flex md:flex-row flex-col mt-16 md:ml-[15em] md:mr-[15em] items-center">
-        <div className="mt-2 md:mr-10">
+        {/* <div className="mt-2 md:mr-10">
           <Avatar className="h-32 w-32">
             <AvatarImage src="https://github.com/shadcn.png" alt="@avatar" />
             <AvatarFallback />
           </Avatar>
-        </div>
+        </div> */}
         <div className="md:flex-col md:w-full mt-1 md:mt-0">
           <div className="flex items-center mb-2 md:mb-0">
-            <div className="flex flex-col">
-              <h1 className="font-bold text-xl md:text-5xl">
-                {publicProfileUser ? publicProfileUser.username : "none"}
-              </h1>
-
-              <div
-                className="mt-2 text-stone-600"
-                hidden={!publicProfileUser.telephoneNumber}
-              >
-                Mobile number: {publicProfileUser.telephoneNumber}
+            <div className="flex border-2 border-black">
+              <div className="mt-2 mr-4 md:mr-10">
+                <Avatar className="h-32 w-32">
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@avatar"
+                  />
+                  <AvatarFallback />
+                </Avatar>
               </div>
+              <div className="flex flex-col">
+                <div className="flex">
+                  <h1 className="font-bold text-xl md:text-5xl">
+                    {publicProfileUser ? publicProfileUser.username : "none"}
+                  </h1>
 
-              <div
-                className="mt-2 text-stone-600"
-                hidden={!publicProfileUser.website}
-              >
-                Website: {publicProfileUser.website}
+                  {publicProfileUser.id === currentUser.id ? (
+                    <Link href="/private-profile?type=update">
+                      <Button variant="ghost" className="mt-1.5">
+                        <Pencil1Icon width="23" height="23" />
+                      </Button>
+                    </Link>
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
+
+                <ShowMoreDetailsPublicProfile
+                  publicProfileUser={publicProfileUser}
+                />
               </div>
             </div>
-            {publicProfileUser.id === currentUser.id ? (
-              <Link href="/private-profile?type=update">
-                <Button variant="ghost" className="">
-                  <Pencil1Icon width="23" height="23" />
-                </Button>
-              </Link>
-            ) : (
-              <div></div>
-            )}
           </div>
           <Textarea
             className="resize-none mt-4"
@@ -86,7 +87,6 @@ export default async function ProfilePage({ searchParams }) {
               publicProfileUser ? publicProfileUser.biography : "none"
             }
             readOnly={true}
-            
           />
         </div>
       </div>
---
+++ app/components/showMoreDetailsPublicProfile.js
@@ -0,0 +1,32 @@
+"use client";
+
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
+
+export default function ShowMoreDetailsPublicProfile({ publicProfileUser }) {
+  const [showMoreDetails, setShowMoreDetails] = useState(false);
+
+  return (
+    <>
+      <DotsHorizontalIcon
+        onClick={() => {
+          setShowMoreDetails(!showMoreDetails);
+        }}
+        hidden={
+          !publicProfileUser.telephoneNumber || !publicProfileUser.website
+        }
+        width={20}
+        height={20}
+        className="md:mt-2"
+      />
+
+      <div className="mt-2 text-stone-600" hidden={!showMoreDetails}>
+        Mobile number: {publicProfileUser.telephoneNumber}
+      </div>
+
+      <div className="mt-2 text-stone-600" hidden={!showMoreDetails}>
+        Website: {publicProfileUser.website}
+      </div>
+    </>
+  );
+}
