48b5e1ed893964dc2ee0d6e0b5dca301c79da4b8
Graffioh <bregliascuola2002@gmail.com>
2024-03-11 14:49:02 +0100

    Add realtime timer in auction-details

--- app/(auction)/auction-details/page.js
+++ app/(auction)/auction-details/page.js
@@ -16,6 +16,7 @@ import PlaceOfferDialog from "@/app/components/placeOfferDialog";
 import { cn } from "@/lib/utils";
 import { useUserContext } from "@/app/providers/userProvider";
 import LoadingSpinner from "@/app/components/loadingSpinner";
+import AuctionTimer from "@/app/components/auctionTimer";
 
 export default function AuctionDetailsPage({ searchParams }) {
   const authToken = useCookies().get("auth-token");
@@ -68,6 +69,34 @@ export default function AuctionDetailsPage({ searchParams }) {
       ? "69:69:69"
       : "00:00:00";
 
+  function generateDeadline(deadline, time) {
+    const deadlineTime = time ? time.split(":") : "";
+    const hours = parseInt(deadlineTime[0]);
+    const minutes = parseInt(deadlineTime[1]);
+    const seconds = parseInt(deadlineTime[2]);
+    deadline.setHours(hours, minutes, seconds);
+
+    return deadline;
+  }
+
+  const fixedTimeDeadline = new Date(currentAuction.expireDate);
+  const fixedTimeDeadlineTimer = generateDeadline(
+    fixedTimeDeadline,
+    currentAuction.expireTime
+  );
+
+  const englishDeadline = new Date();
+  const englishDeadlineTimer = generateDeadline(
+    englishDeadline,
+    currentAuction.baseOfferTimer
+  );
+
+  const descendingDeadline = new Date();
+  const descendingDeadlineTimer = generateDeadline(
+    descendingDeadline,
+    currentAuction.baseDecrementTimer
+  );
+
   return (
     <>
       <div className="flex flex-col md:flex-row mx-20 justify-between">
@@ -82,7 +111,8 @@ export default function AuctionDetailsPage({ searchParams }) {
             width={410}
             height={180}
           />
-          <div className="flex">
+          {/* IDEA TO SHOW OTHER AUCTION IMAGES*/}
+          {/* <div className="flex">
             <Image
               alt="auction-image-1"
               className="rounded-lg mx-1"
@@ -104,7 +134,7 @@ export default function AuctionDetailsPage({ searchParams }) {
               width={130}
               height={80}
             />
-          </div>
+          </div> */}
         </div>
         <div className="flex flex-col max-w-2xl my-8">
           <div className="px-10 bg-stone-200 rounded-xl shadow-[0px_4px_16px_rgba(17,17,26,0.2),_0px_8px_24px_rgba(17,17,26,0.2),_0px_16px_56px_rgba(17,17,26,0.2)]">
@@ -114,7 +144,7 @@ export default function AuctionDetailsPage({ searchParams }) {
               </Label>
 
               <Link href={"/public-profile?id=" + currentAuction.idUserAccount}>
-                <Avatar className="h-32 w-32">
+                <Avatar className="h-32 w-32 hover:opacity-90">
                   <AvatarImage
                     src="https://i.scdn.co/image/ab676161000051744e975208a929cd58c552c55b"
                     alt="@avatar"
@@ -122,14 +152,14 @@ export default function AuctionDetailsPage({ searchParams }) {
                   <AvatarFallback />
                 </Avatar>
               </Link>
-              <div className="flex w-full mt-7 justify-between mx-8">
-                <div className="flex flex-col">
+              <div className="flex flex-col md:flex-row w-full mt-7 justify-between mx-8">
+                <div className="flex flex-col w-full md:mr-4">
                   <Label className="flex mb-2">
                     Current offer<div className="text-red-500"></div>
                   </Label>
                   <div className="flex">
                     <Link href={"/public-profile?id=" + highestOfferUserId}>
-                      <Avatar className="h-8 w-8 mt-0.5 mr-2.5">
+                      <Avatar className="h-8 w-8 mt-0.5 mr-2.5 hover:opacity-90">
                         <AvatarImage
                           src="https://i.scdn.co/image/ab676161000051744e975208a929cd58c552c55b"
                           alt="@avatar"
@@ -138,7 +168,7 @@ export default function AuctionDetailsPage({ searchParams }) {
                       </Avatar>
                     </Link>
                     <Input
-                      className="max-w-[10em] h-9 bg-white"
+                      className="max-w-[10em] h-9 bg-white mb-4 md:mb-0"
                       type="text"
                       placeholder="Placeholder"
                       defaultValue={currentAuction.currentOffer}
@@ -147,26 +177,38 @@ export default function AuctionDetailsPage({ searchParams }) {
                   </div>
                 </div>
 
-                <div className="flex flex-col">
+                <div className="flex flex-col w-full md:ml-4">
                   <Label className="flex mb-2">
                     Timer<div className="text-red-500"></div>
                   </Label>
-                  <Input
+                  {/* <Input
                     className="max-w-[20em] h-9 bg-white"
                     type="text"
                     placeholder="Placeholder"
                     defaultValue={timerValue}
                     readOnly
-                  />
+                  /> */}
+                  <div className="text-base bg-white py-1 h-9 px-3 rounded border border-input">
+                    <AuctionTimer
+                      deadline={
+                        currentAuction.auctionType === "english"
+                          ? englishDeadlineTimer
+                          : currentAuction.auctionType === "descending"
+                          ? descendingDeadlineTimer
+                          : fixedTimeDeadlineTimer
+                      }
+                      auction={currentAuction}
+                    />
+                  </div>
                 </div>
               </div>
-              <div className="flex w-full mt-8 justify-between">
-                <div className="flex flex-col">
+              <div className="flex flex-col md:flex-row w-full mt-4 md:mt-8 justify-between">
+                <div className="flex flex-col w-full md:mr-4">
                   <Label className="flex mb-2">
                     Category<div className="text-red-500"></div>
                   </Label>
                   <Input
-                    className="max-w-[20em] h-9 bg-white"
+                    className="h-9 bg-white mb-4 md:mb-0"
                     type="text"
                     placeholder="Placeholder"
                     defaultValue={currentAuction.auctionCategory}
@@ -174,12 +216,12 @@ export default function AuctionDetailsPage({ searchParams }) {
                   />
                 </div>
 
-                <div className="flex flex-col">
+                <div className="flex flex-col w-full md:ml-4">
                   <Label className="flex mb-2">
                     Quality<div className="text-red-500"></div>
                   </Label>
                   <Input
-                    className="max-w-[20em] h-9 bg-white"
+                    className="h-9 bg-white"
                     type="text"
                     placeholder="Placeholder"
                     defaultValue={currentAuction.auctionQuality}
