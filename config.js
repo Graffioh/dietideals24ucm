34a1c7f1038633b04bd090e493a992c7b7c20186
Graffioh <bregliascuola2002@gmail.com>
2024-03-16 11:27:28 +0100

    Remove ternary in configs for prod and dev

--- config.js
+++ config.js
@@ -1,15 +1,6 @@
-const isProd = process.env.NEXT_ENV === "prod";
-
-console.log("IS PROD? " + isProd)
 
 const config = {
-  apiUrl: isProd
-    ? process.env.NEXT_PUBLIC_API_URL_PROD
-    : process.env.NEXT_PUBLIC_API_URL_DEV,
+  apiUrl: process.env.NEXT_PUBLIC_API_URL
 };
-
-console.log("process.env.NEXT_PUBLIC_API_URL_PROD = " + process.env.NEXT_PUBLIC_API_URL_PROD)
-console.log("process.env.NEXT_PUBLIC_API_URL_DEV = " + process.env.NEXT_PUBLIC_API_URL_DEV)
-console.log("CONFIG API URL: " + config.apiUrl)
 
 module.exports = config;
--- next.config.js
+++ next.config.js
@@ -8,19 +8,18 @@ module.exports = {
     domains: ["m.media-amazon.com"],
   },
   async rewrites() {
-    const isProd = process.env.NEXT_ENV === "prod";
     return [
       {
+        source: "/dietideals24.vercel.app/api/:path*",
+        destination: "https://server-dietideals24.onrender.com/:path*"
+      },
+      {
         source: "/api/:path*",
-        destination: isProd
-          ? "https://server-dietideals24.onrender.com/:path*"
-          : "https://server-dietideals24-render-dev.onrender.com/:path*",
+        destination: "https://server-dietideals24-render-dev.onrender.com/:path*"
       },
       {
         source: "/login/oauth2/:path*",
-        destination: isProd
-          ? process.env.NEXT_PUBLIC_API_URL_PROD + "/login/oauth2/:path*"
-          : process.env.NEXT_PUBLIC_API_URL_DEV + "/login/oauth2/:path*",
+        destination: process.env.NEXT_PUBLIC_API_URL + "/login/oauth2/:path*"
       },
     ];
   },
