f64ddc2f6bf2e14a8f1f9beae2548b6366c88954
Graffioh <bregliascuola2002@gmail.com>
2024-04-06 14:08:48 +0200

    Add comments to tests for better readability

--- server-dietideals24/src/test/java/com/ucm/serverdietideals24/UserAccountTests.java
+++ server-dietideals24/src/test/java/com/ucm/serverdietideals24/UserAccountTests.java
@@ -29,19 +29,26 @@ public class UserAccountTests {
     @ParameterizedTest
     @CsvSource({
             "Umberto, Breglia, brr_777, breglia@breglia.com, brbr172, 1990-05-15, true",
-            "Andr£w, Ta$e, at, at@at.com, at1pw, 2030-09-20, false",
+            "Andr\\w, Ta\\$e, at, at@at.com, at1pw, 2030-09-20, false",
             "Guts, Casca, gclove232, gutscasca.com, 502er, 2025-01-01, false",
             "Anna, Oxa, aoxa, xo@xo.com, 123pw, 1969-11-11, true",
     })
     public void testIsUserAccountInfoValidForRegistration(String firstName, String lastName, String username,
             String email, String password, String birthDateStr, boolean expected) throws Exception {
+        
         // Arrange
+        // ************************************************
         UserAccount userAccount = new UserAccount(new Date().getTime(), firstName, lastName, username, password, email, parseDate(birthDateStr));
+        // ************************************************
 
         // Act
+        // ************************************************
         Boolean isValidResult = userValidator.isUserValidForRegistration(userAccount);
-        
+        // ************************************************
+
         // Assert
+        // ************************************************
         assertEquals(expected, isValidResult);
+        // ************************************************
     }
 }
