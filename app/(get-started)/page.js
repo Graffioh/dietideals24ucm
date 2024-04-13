cf30824f29c01fa757cc2d0c4f1727c55883f11d
Graffioh <bregliascuola2002@gmail.com>
2024-04-13 21:01:02 +0200

    Change get-started text

--- app/(get-started)/page.js
+++ app/(get-started)/page.js
@@ -4,13 +4,12 @@ export default function GetStartedPage() {
   return (
     <>
       <div className="flex flex-col h-[80vh] justify-center items-center w-full">
-        <div className="mb-4 text-3xl md:text-7xl font-extrabold">
+        <div className="mb-4 ml-7 text-3xl md:text-7xl font-extrabold">
           <h1>Sell and buy with just a few clicks. </h1>
         </div>
-        <div className="mb-4 italic ml-8 text-xl md:text-4xl">
+        <div className="mb-4 italic ml-5 text-xl md:text-4xl">
           <p>
-            It&apos;s easy and fast to use. Are you ready? Let&apos;s get
-            started!
+            It&apos;s easy and fast to use. Are you ready?
           </p>
         </div>
         <Link href="/home">
