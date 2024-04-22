abfb0894b4d9be820e3a77d016d19c7abfe6f60d
Graffioh <bregliascuola2002@gmail.com>
2024-04-22 15:28:09 +0200

    Split auction validation based on type

--- server-dietideals24/src/main/java/com/ucm/serverdietideals24/Util/AuctionValidatorUtil.java
+++ server-dietideals24/src/main/java/com/ucm/serverdietideals24/Util/AuctionValidatorUtil.java
@@ -10,8 +10,9 @@ import com.ucm.serverdietideals24.Models.Auction;
 public class AuctionValidatorUtil {
 
     private static final Pattern VALID_TITLE_PATTERN = Pattern.compile("^[a-zA-Z\\s]+$");
-    private static final Pattern VALID_IMAGE_EXTENSION_PATTERN = Pattern.compile("(.*?)\\.(?:jpe?g|png)$", Pattern.CASE_INSENSITIVE);
-    
+    private static final Pattern VALID_IMAGE_EXTENSION_PATTERN = Pattern.compile("(.*?)\\.(?:jpe?g|png)$",
+            Pattern.CASE_INSENSITIVE);
+
     private static boolean isExpireDateValid(Date expireDate) {
         // Check if the date is in the past
         LocalDate currentDate = LocalDate.now();
@@ -29,41 +30,75 @@ public class AuctionValidatorUtil {
         return VALID_IMAGE_EXTENSION_PATTERN.matcher(image).matches();
     }
 
-    public boolean isAuctionValid(Auction auction) {
+    private boolean isFixedTimeAuctionValid(Auction auction) {
         String title = auction.getAuctionName();
-        String type = auction.getAuctionType().toString();
         String image = auction.getAuctionImages();
 
         if (!isValidTitle(title) || !isValidImageExtension(image)) {
             return false;
         }
 
-        if (type.equals("fixedtime")) {
-            Date expireDate = auction.getExpireDate();
-            Float minimumAcceptablePrice = auction.getMinimumAcceptablePrice();
+        Date expireDate = auction.getExpireDate();
+        Float minimumAcceptablePrice = auction.getMinimumAcceptablePrice();
 
-            if (!isExpireDateValid(expireDate) || minimumAcceptablePrice < 0 || minimumAcceptablePrice > 9999) {
-                return false;
-            }
+        if (!isExpireDateValid(expireDate) || minimumAcceptablePrice < 0 || minimumAcceptablePrice > 9999) {
+            return false;
+        }
+        
+        return true;
+    }
 
-        } else if (type.equals("english")) {
-            Float startPrice = auction.getStartPrice();
-            Float riseThresholdPrice = auction.getRiseThreshold();
+    private boolean isEnglishAuctionValid(Auction auction) {
+        String title = auction.getAuctionName();
+        String image = auction.getAuctionImages();
 
-            if (startPrice < 0 || startPrice > 9999 || riseThresholdPrice < 0) {
-                return false;
-            }
-        } else {
-            Float startPrice = auction.getStartPrice();
-            Float decrementAmount = auction.getDecrementAmount();
-            Float endPrice = auction.getEndPrice();
+        if (!isValidTitle(title) || !isValidImageExtension(image)) {
+            return false;
+        }
 
-            if (decrementAmount > startPrice || endPrice > startPrice || startPrice < 0 || startPrice > 9999 || decrementAmount < 0
-                    || endPrice < 0) {
-                return false;
-            }
+        Float startPrice = auction.getStartPrice();
+        Float riseThresholdPrice = auction.getRiseThreshold();
+
+        if (startPrice < 0 || startPrice > 9999 || riseThresholdPrice < 0) {
+            return false;
         }
 
         return true;
+    }
+
+    private boolean isDescendingAuctionValid(Auction auction) {
+        String title = auction.getAuctionName();
+        String image = auction.getAuctionImages();
+
+        if (!isValidTitle(title) || !isValidImageExtension(image)) {
+            return false;
+        }
+
+        Float startPrice = auction.getStartPrice();
+        Float decrementAmount = auction.getDecrementAmount();
+        Float endPrice = auction.getEndPrice();
+
+        if (decrementAmount > startPrice || endPrice > startPrice || startPrice < 0 || startPrice > 9999
+                || decrementAmount < 0
+                || endPrice < 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isAuctionValid(Auction auction) {
+        String type = auction.getAuctionType().toString();
+
+        switch (type) {
+            case "fixed-time":
+                return isFixedTimeAuctionValid(auction);
+            case "english":
+                return isEnglishAuctionValid(auction);
+            case "descending":
+                return isDescendingAuctionValid(auction);
+            default:
+                return false;
+        }
     }
 }
