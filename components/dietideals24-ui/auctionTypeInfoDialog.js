9705212275ee4bc9ae8cf4a8c7c1700e97c9eced
Graffioh <bregliascuola2002@gmail.com>
2024-05-04 13:05:23 +0200

    Add auction type info dialog in insert-auction for better clarity

---
+++ components/dietideals24-ui/auctionTypeInfoDialog.js
@@ -0,0 +1,84 @@
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/UTJnUXifJWh
+ * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
+ */
+import { Button } from "@/components/shadcn-ui/button";
+import {
+  AlertDialogTrigger,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogCancel,
+  AlertDialogAction,
+  AlertDialogFooter,
+  AlertDialogContent,
+  AlertDialog,
+} from "@/components/shadcn-ui/alert-dialog";
+
+import { InfoCircledIcon } from "@radix-ui/react-icons";
+
+export default function Component() {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="none">
+          <InfoCircledIcon width="22" height="22" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogTitle>Fixed time auction</AlertDialogTitle>
+        <AlertDialogDescription>
+          The auction starts with a <a className="font-bold">start price</a>. If
+          the expire date is reached and no bids
+          have been placed, or the highest bid does not exceed the{" "}
+          <a className="font-bold">reserve price</a>, the auction is considered
+          failed. In any case, it ends once the <a className="font-bold">expire date</a> has been reached.
+          {/* <div className="mt-2">
+            <ul>
+              <li>Start price:</li>
+              <li>Expire date: </li>
+              <li>Reserve price:</li>
+            </ul>
+          </div> */}
+        </AlertDialogDescription>
+        <AlertDialogTitle>English auction</AlertDialogTitle>
+        <AlertDialogDescription>
+          The auction starts with a <a className="font-bold">start price</a>,
+          and bidding continues until the{" "}
+          <a className="font-bold">offer timer</a> expires. To participate,
+          bidders must submit offers that increase the current price by at least
+          the <a className="font-bold">rise threshold price</a>, which is set at
+          €X (e.g., if the current bid is €10 and the rise threshold is €5, the
+          next bidder must offer at least €15 to be considered).
+          {/* <div className="mt-2">
+            <ul>
+              <li>Start price:</li>
+              <li>Expire date: </li>
+              <li>Reserve price:</li>
+            </ul>
+          </div> */}
+        </AlertDialogDescription>
+        <AlertDialogTitle>Descending auction</AlertDialogTitle>
+        <AlertDialogDescription>
+          The auction start with a <a className="font-bold">start price</a>. From there, bidding continues
+          until either a new offer is submitted or the auction reaches{" "}
+          <a className="font-bold">end price</a>. The{" "}
+          <a className="font-bold">decrement timer</a> counts down, and every
+          time it expires, it resets and automatically lowers the current offer
+          by a set <a className="font-bold">decrement amount</a>.
+          {/* <div className="mt-2">
+            <ul>
+              <li>Start price:</li>
+              <li>Expire date: </li>
+              <li>Reserve price:</li>
+            </ul>
+          </div> */}
+        </AlertDialogDescription>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Ok</AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
